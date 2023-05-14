5e157c870211744f0a60f9d1a04a11854ddc12f6
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-05-14 21:56:00 +0200

    feat: use better regex patterns to get context and highlight word

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -7,7 +7,10 @@ export class HighlightPipe implements PipeTransform {
 
   transform(value: any, args: string): unknown {
     if(!value || !args) return value;
-    const re = new RegExp("("+args+")", 'igm');
+    // the search term has to be enclosed by word boundaries. as the standard word boundaries (\b)
+    // do not include special characters like àèì, the regex has to be written manually. see:
+    // https://stackoverflow.com/a/56945933
+    const re = new RegExp("(?<![äöüÄÖÜàéèòìÀÉÈÒÌ\\w])("+args+")(?![äöüÄÖÜàéèòìÀÉÈÒÌ\\w])", 'gm');
     value= value.replace(re, '<span class="highlighted-text">$1</span>');
     return value;
   }
--- src/app/utils/text.utils.ts
+++ src/app/utils/text.utils.ts
@@ -1,7 +1,11 @@
 export default class TextUtils {
     static getContext(searchTerm: string, text: string): string | undefined {
-        const regExpString = String.raw`(([a-z]+[^a-z]+)|([^a-z]+[a-z]+)){0,4}${searchTerm}(([a-z]+[^a-z]+)|([^a-z]+[a-z]+)){0,4}`;
-        const regExp = new RegExp(regExpString, 'i');
+        // this regex looks up for 3 words before the searchTerm word and 3 words after it
+        // the search term has to be enclosed by word boundaries. as the standard word boundaries (\b)
+        // do not include special characters like àèì, the regex has to be written manually. see:
+        // https://stackoverflow.com/a/56945933
+        const regExpString = String.raw`(([a-z]+[^a-z]+)|([^a-z]+[a-z]+)){0,3}(?<![äöüÄÖÜàéèòìÀÉÈÒÌ\w])(${searchTerm})(?![äöüÄÖÜàéèòìÀÉÈÒÌ\w])(([a-z]+[^a-z]+)|([^a-z]+[a-z]+)){0,3}`;
+        const regExp = new RegExp(regExpString, 'gm');
         return text.match(regExp)?.[0];
     }
 }
