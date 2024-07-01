7d7b93421966e2ae3fb0f279de15945f87e8347d
Gion-Andri Cantieni <gion-andri@gmx.ch>
2024-07-01 16:26:52 +0200

    feat: improve performance for initial correction

--- src/app/spellchecker-inline/spellchecker-inline.component.ts
+++ src/app/spellchecker-inline/spellchecker-inline.component.ts
@@ -10,6 +10,9 @@ import { UserDictionaryService } from '../services/user-dictionary.service';
 export class SpellcheckerInlineComponent implements OnInit {
 
   isSpellchecking = false;
+  executionTime = "";
+
+  private start: number = 0;
 
   constructor(
       private spellcheckerService: SpellcheckerService,
@@ -26,26 +29,32 @@ export class SpellcheckerInlineComponent implements OnInit {
     this.isSpellchecking = true;
 
     return Word.run(async (context) => {
+      this.start = performance.now();
       OfficeExtension.config.extendedErrorLogging = true;
       const body = context.document.body;
       try {
         context.load(body.paragraphs);
         await context.sync();
 
+        this.start = performance.now();
         const paragraphCollection = body.paragraphs.load({
-          text: true,
           uniqueLocalId: true,
+          text: true,
         });
+        await context.sync();
+
+        await this.deleteAllAnnotations(context, paragraphCollection);
         for (const paragraph of paragraphCollection.items) {
-          const paragraphText = paragraph.text;
-          await this.spellcheckParagraph(context, paragraphText, paragraph.uniqueLocalId);
-          console.log(`${paragraph.uniqueLocalId} - ${paragraph.text}`);
+          await this.spellcheckParagraph(context, paragraph);
         }
+        await context.sync();
       } catch (e) {
         // @ts-ignore
         console.error(e.message, e.debugInfo);
       } finally {
         this.isSpellchecking = false;
+        const end = performance.now();
+        this.executionTime = `Execution time: ${end - this.start} ms`;
       }
     });
   }
@@ -57,23 +66,18 @@ export class SpellcheckerInlineComponent implements OnInit {
       context.document.onParagraphChanged.add(this.paragraphChanged.bind(that));
       context.document.onParagraphDeleted.add(this.paragraphDeleted.bind(that));
       await context.sync();
-
-      console.log("Added event handler for paragraph changes");
     });
   }
 
   private async paragraphAdded(event: Word.ParagraphAddedEventArgs) {
     await Word.run(async (context) => {
       await context.sync();
-      console.log(event);
-
       for (let id of event.uniqueLocalIds) {
-        let paragraph = context.document.getParagraphByUniqueLocalId(id);
-        paragraph.load();
-        await paragraph.context.sync();
-
-        console.log(`${event.type} - ${paragraph.uniqueLocalId} - ${paragraph.text}`);
-        await this.spellcheckParagraph(context, paragraph.text, paragraph.uniqueLocalId);
+        const paragraph = context.document.getParagraphByUniqueLocalId(id);
+        paragraph.load("text");
+        await context.sync();
+        await this.spellcheckParagraph(context, paragraph);
+        await context.sync();
       }
     });
   }
@@ -81,14 +85,11 @@ export class SpellcheckerInlineComponent implements OnInit {
   private async paragraphChanged(event: Word.ParagraphChangedEventArgs) {
     await Word.run(async (context) => {
       for (let id of event.uniqueLocalIds) {
-        let paragraph = context.document.getParagraphByUniqueLocalId(id);
-        paragraph.load();
-        await paragraph.context.sync();
-        paragraph.parentBody.load();
-        await paragraph.parentBody.context.sync();
-
-        console.log(`${event.type} - ${paragraph.uniqueLocalId} - ${paragraph.text}- ${paragraph.parentBody.type}`);
-        await this.spellcheckParagraph(context, paragraph.text, paragraph.uniqueLocalId);
+        const paragraph = context.document.getParagraphByUniqueLocalId(id);
+        paragraph.load("text");
+        await context.sync();
+        await this.spellcheckParagraph(context, paragraph);
+        await context.sync();
       }
     });
   }
@@ -96,14 +97,13 @@ export class SpellcheckerInlineComponent implements OnInit {
   private async paragraphDeleted(event: Word.ParagraphDeletedEventArgs) {
     await Word.run(async (context) => {
       for (let id of event.uniqueLocalIds) {
-        console.log(`${event.type} - ${id}`);
       }
     });
   }
 
-  private async spellcheckParagraph(context: Word.RequestContext, paragraphText: string, paragraphId: string) {
-    await this.deleteExistingAnnotations(context, paragraphId);
-    const errs = await this.spellcheckerService.proofreadText(paragraphText);
+  private async spellcheckParagraph(context: Word.RequestContext, paragraph: Word.Paragraph) {
+    // this.start = performance.now();
+    const errs = await this.spellcheckerService.proofreadText(paragraph.text);
     const critiques: Word.Critique[] = [];
     for await (const e of errs) {
       if (this.userDictionaryService.isInDictionary(e.word)) {
@@ -129,27 +129,26 @@ export class SpellcheckerInlineComponent implements OnInit {
       const annotationSet: Word.AnnotationSet = {
         critiques: critiques
       };
-      const paragraph = context.document.getParagraphByUniqueLocalId(paragraphId)
-      paragraph.load();
-      await paragraph.context.sync();
-      const annotationIds = paragraph.insertAnnotations(annotationSet);
-      console.log(annotationIds)
-      await context.sync();
-      console.log('inserted annotations', annotationSet)
+      paragraph.insertAnnotations(annotationSet);
     }
   }
 
-  async deleteExistingAnnotations(context: Word.RequestContext, paragraphId: string) {
-    const paragraph = context.document.getParagraphByUniqueLocalId(paragraphId)
-    const annotations: Word.AnnotationCollection = paragraph.getAnnotations();
-    annotations.load("id");
+  async deleteAllAnnotations(context: Word.RequestContext, paragraphs: Word.ParagraphCollection) {
+    const allAnnotations: Word.AnnotationCollection[] = [];
+    for (const paragraph of paragraphs.items) {
+      const annotations: Word.AnnotationCollection = paragraph.getAnnotations();
+      annotations.load("id");
+      allAnnotations.push(annotations);
+    }
 
     await context.sync();
 
-    for (let i = 0; i < annotations.items.length; i++) {
-      const annotation: Word.Annotation = annotations.items[i];
-      annotation.delete();
-    }
+    allAnnotations.forEach(annotations => {
+      for (let i = 0; i < annotations.items.length; i++) {
+        const annotation: Word.Annotation = annotations.items[i];
+        annotation.delete();
+      }
+    });
 
     await context.sync();
   }
