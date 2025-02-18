1c4708742d63ca7cb3f0c049eed6fbd801eaf525
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-02-18 17:27:19 +0100

    feat: disable context function in settings for inline version

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,60 +1,68 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { SettingsService } from "../services/settings.service";
 import { Language } from "../data/language";
 import { Subscription } from "rxjs";
 import { SpellcheckerService } from "../services/spellchecker.service";
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 // this loads package.json
 // then you destructure that object and take out the 'version' property from it
 // and finally with ': appVersion' you rename it to const appVersion
-const {version: appVersion} = require('../../../package.json');
+const { version: appVersion } = require("../../../package.json");
 
 @Component({
-    selector: 'app-settings',
-    templateUrl: './settings.component.html',
-    styleUrls: ['./settings.component.scss'],
-    imports: [FormsModule]
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.scss"],
+  imports: [FormsModule],
 })
 export class SettingsComponent implements OnInit, OnDestroy {
+  @Input()
+  wordApiLevel18Supported: boolean = false;
 
-    language: Language = 'rumantschgrischun';
+  language: Language = "rumantschgrischun";
 
-    showContext: boolean = true;
+  showContext: boolean = true;
 
-    public appVersion = '-';
+  public appVersion = "-";
 
-    private languageSubscription?: Subscription;
-    private showContextSubscription?: Subscription;
+  private languageSubscription?: Subscription;
+  private showContextSubscription?: Subscription;
 
-    constructor(private settingsService: SettingsService, public spellcheckerService: SpellcheckerService) {
+  constructor(
+    private settingsService: SettingsService,
+    public spellcheckerService: SpellcheckerService
+  ) {}
+
+  ngOnInit() {
+    this.appVersion = appVersion;
+    this.languageSubscription = this.settingsService
+      .getLanguageObservable()
+      .subscribe((lng) => {
+        this.language = lng;
+      });
+
+    this.showContextSubscription = this.settingsService
+      .getShowContextObservable()
+      .subscribe((ctx) => {
+        this.showContext = ctx;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
     }
 
-    ngOnInit() {
-        this.appVersion = appVersion;
-        this.languageSubscription = this.settingsService.getLanguageObservable().subscribe(lng => {
-            this.language = lng;
-        });
-
-        this.showContextSubscription = this.settingsService.getShowContextObservable().subscribe(ctx => {
-            this.showContext = ctx;
-        });
+    if (this.showContextSubscription) {
+      this.showContextSubscription.unsubscribe();
     }
+  }
 
-    ngOnDestroy() {
-        if (this.languageSubscription) {
-            this.languageSubscription.unsubscribe();
-        }
+  languageSelected(lng: Language) {
+    this.settingsService.setLanguage(lng);
+  }
 
-        if (this.showContextSubscription) {
-            this.showContextSubscription.unsubscribe();
-        }
-    }
-
-    languageSelected(lng: Language) {
-        this.settingsService.setLanguage(lng);
-    }
-
-    showContextChanged(value: boolean) {
-        this.settingsService.setShowContext(value);
-    }
+  showContextChanged(value: boolean) {
+    this.settingsService.setShowContext(value);
+  }
 }
