036a1ec44c2572e8452f8fc6ea8c800f0eb127ab
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-02-18 13:35:56 +0100

    chore: bootstrap the application using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { TabType } from "./data/tabs";
+import { NgIf } from '@angular/common';
+import { SpellcheckerLegacyComponent } from './spellchecker-legacy/spellchecker-legacy.component';
+import { SpellcheckerInlineComponent } from './spellchecker-inline/spellchecker-inline.component';
+import { SettingsComponent } from './settings/settings.component';
+import { IgnoredWordsComponent } from './ignored-words/ignored-words.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    imports: [NgIf, SpellcheckerLegacyComponent, SpellcheckerInlineComponent, SettingsComponent, IgnoredWordsComponent, TabsComponent]
 })
 export class AppComponent {
-  selectedTab: TabType = 'spellchecker';
+    selectedTab: TabType = 'spellchecker';
 
-  public wordApiLevel18Supported = false;
+    public wordApiLevel18Supported = false;
 
-  constructor() {
-    this.wordApiLevel18Supported = Office.context.requirements.isSetSupported("WordApi", "1.8");
-    if (!this.wordApiLevel18Supported) {
-      console.warn('wordAPI level 1.7 not supported. Mark errors inline disabled');
+    constructor() {
+        this.wordApiLevel18Supported = Office.context.requirements.isSetSupported("WordApi", "1.8");
+        if (!this.wordApiLevel18Supported) {
+            console.warn('wordAPI level 1.7 not supported. Mark errors inline disabled');
+        }
     }
-  }
 
-  tabChanged(type: TabType) {
-    this.selectedTab = type;
-  }
+    tabChanged(type: TabType) {
+        this.selectedTab = type;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,55 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { provideDialogConfig } from '@ngneat/dialog';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { VirtualScrollerModule } from '@iharbeck/ngx-virtual-scroller';
+import { AppRoutingModule } from './app/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MatomoModule } from 'ngx-matomo-client/core';
+import { ToastrModule } from 'ngx-toastr';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 Office.initialize = () => {
-  // Bootstrap the app
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch(error => console.error(error));
+    // Bootstrap the app
+    bootstrapApplication(AppComponent, {
+        providers: [
+            importProvidersFrom(
+                BrowserModule,
+                NgxSpinnerModule,
+                VirtualScrollerModule,
+                AppRoutingModule,
+                FormsModule,
+                MatomoModule.forRoot({
+                    siteId: "10", // your Matomo's site ID (find it in your Matomo's settings)
+                    trackerUrl: "https://www.statistica.pledarigrond.ch", // your matomo server root url
+                }),
+                ToastrModule.forRoot({
+                    positionClass: "toast-bottom-center",
+                    maxOpened: 1,
+                    autoDismiss: true,
+                })
+            ),
+            provideDialogConfig({
+                closeButton: true,
+                enableClose: false,
+                backdrop: true,
+                resizable: false,
+                draggable: false,
+                windowClass: "modal-dialog",
+                width: "calc(100vw - 40px)",
+            }),
+            provideHttpClient(withInterceptorsFromDi()),
+            provideAnimations(),
+        ]
+    })
+        .catch(error => console.error(error));
 };
