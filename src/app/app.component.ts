90dc7f593707bf5e3f3493e8d65133d0acbf5cbe
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-02-19 11:45:03 +0100

    feat: track api level 1.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { TabType } from "./data/tabs";
 
-import { SpellcheckerLegacyComponent } from './spellchecker-legacy/spellchecker-legacy.component';
-import { SpellcheckerInlineComponent } from './spellchecker-inline/spellchecker-inline.component';
-import { SettingsComponent } from './settings/settings.component';
-import { IgnoredWordsComponent } from './ignored-words/ignored-words.component';
-import { TabsComponent } from './tabs/tabs.component';
+import { SpellcheckerLegacyComponent } from "./spellchecker-legacy/spellchecker-legacy.component";
+import { SpellcheckerInlineComponent } from "./spellchecker-inline/spellchecker-inline.component";
+import { SettingsComponent } from "./settings/settings.component";
+import { IgnoredWordsComponent } from "./ignored-words/ignored-words.component";
+import { TabsComponent } from "./tabs/tabs.component";
+import { MatomoTracker } from "ngx-matomo-client";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [SpellcheckerLegacyComponent, SpellcheckerInlineComponent, SettingsComponent, IgnoredWordsComponent, TabsComponent]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  imports: [
+    SpellcheckerLegacyComponent,
+    SpellcheckerInlineComponent,
+    SettingsComponent,
+    IgnoredWordsComponent,
+    TabsComponent,
+  ],
 })
 export class AppComponent {
-    selectedTab: TabType = 'spellchecker';
+  selectedTab: TabType = "spellchecker";
 
-    public wordApiLevel18Supported = false;
+  public wordApiLevel18Supported = false;
 
-    constructor() {
-        this.wordApiLevel18Supported = Office.context.requirements.isSetSupported("WordApi", "1.8");
-        if (!this.wordApiLevel18Supported) {
-            console.warn('wordAPI level 1.7 not supported. Mark errors inline disabled');
-        }
+  constructor(private matomoTracker: MatomoTracker) {
+    this.wordApiLevel18Supported = Office.context.requirements.isSetSupported(
+      "WordApi",
+      "1.8"
+    );
+    if (!this.wordApiLevel18Supported) {
+      console.warn(
+        "wordAPI level 1.8 not supported. Mark errors inline disabled"
+      );
     }
+    this.matomoTracker.trackEvent(
+      "Stats",
+      "Supports WordAPI 1.8",
+      undefined,
+      this.wordApiLevel18Supported ? 1 : 0
+    );
+  }
 
-    tabChanged(type: TabType) {
-        this.selectedTab = type;
-    }
+  tabChanged(type: TabType) {
+    this.selectedTab = type;
+  }
 }
